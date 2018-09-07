a9f10ebe8695658b6d37185a349a1ed0e6846f5b
MariahAdams <mariah.adams23@yahoo.com>
2018-09-07 10:06:46 -0700

    wired up footer component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
+import Footer from './Footer';
 import styles from './App.css';
 
 class App extends Component {
@@ -23,6 +24,10 @@ class App extends Component {
                 <Redirect to="/"/>
               </Switch>
             </main>
+
+            <footer>
+              <Footer/>
+            </footer>
           </div>
         </Fragment>
       </Router>
