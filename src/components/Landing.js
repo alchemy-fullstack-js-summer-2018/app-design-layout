d404d246319ed0f059c8d35ac254fc1940e8aefe
Mark Myers <markalope@gmail.com>
2018-09-07 12:01:33 -0700

    landing page styles complete.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import image from '../assets/image-crop.jpg';
 
 class Landing extends Component {
   render() {
     return (
       <div>
-        <pagehead>
+        <div className="landingcontent">
+          <img src={image}/>
           <h2>Manage Your Day Using Notes.</h2>
-        </pagehead>
-        <p>Manage your notes</p>
+          <p>Store your notes and important reminders using <span className="title">Notes.app</span> and never lose a post-it note again. Your notes are most useful to you if they are well organized and you can find them and use them easily. Try to record them as soon as possible after you have an idea!</p>
+          <p>Start using <span className="title">Notes.app</span> right away by clicking on the Dashboard link.</p>
+        </div>
       </div>
     );
   }
